c60e67d9d9d618336f07649e641498f184a2b1a4
Sean Maguire <scmaguire@users.noreply.github.com>
2018-11-06 19:58:33 -0500

    updated logging messages

--- install/install.sh
+++ install/install.sh
@@ -5,12 +5,15 @@ echo "Installing dotfiles..."
 
 
 # install tmux configuration
+echo "Installing tmux config..."
 cp ../tmux/.tmux.conf ~/.tmux.conf
 
 # install vim configuration
+echo "Installing vim config..."
 cp ../vim/.vimrc ~/.vimrc
 
 # install zsh
+echo "Installing zsh config..."
 cp ../zsh/.zshrc ~/.zshrc
 
 
