a80b6eb75192b04a95688b7191ebb70e76f3e23c
Sean Maguire <scmaguire@users.noreply.github.com>
2019-10-02 21:35:36 -0400

    Updated install script to use symlinks instead of cp command.

--- install.sh
+++ install.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 echo "Installing dotfiles..."
+echo "Creating symlinks..."
 
 # install zsh
 echo "Installing zsh config..."
-cp .zshrc ~/.zshrc
-cp .zsh_aliases ~/.zsh_aliases
+#cp .zshrc ~/.zshrc
+ln -s .zshrc ~/.zshrc
+#cp .zsh_aliases ~/.zsh_aliases
+ln -s .zsh_aliases ~/.zsh_aliases
 
 # install vim configuration
 echo "Installing vim config..."
-cp .vimrc ~/.vimrc
+#cp .vimrc ~/.vimrc
+ln -s .vimrc ~/.vimrc
 
 # install tmux configuration
 echo "Installing tmux config..."
-cp .tmux.conf ~/.tmux.conf
+#cp .tmux.conf ~/.tmux.conf
+ln -s .tmux.conf ~/.tmux.conf
 
 
-# TODO: switch to symlinks?
 
